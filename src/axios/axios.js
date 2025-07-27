ec55194165af9218ff81e30914dededb73fa170b
Muhammad_Rabee <techclub9t@gmail.com>
2025-07-26 21:09:48 -0700

    Update axios.js

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const BaseURLs=['https://saair-backend-production.up.railway.app/api','http://localhost:3200/api']
 
 const axiosinstance=axios.create({
-    baseURL:'http://localhost:3200/api',
+    baseURL:BaseURLs,
     withCredentials:true
 })
 export default axiosinstance
